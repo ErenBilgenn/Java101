4d040c7ab495e28c95e8296025a7e8bad0c9d439
Eren Bilgen <erenbilgenn@gmail.com>
2023-01-06 15:46:47 +0300

    Odev2  tamamlandi.

---
+++ src/Odev2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Odev2 {
+    public static void main(String[] args) {
+        double fiyat , kdv = 0.18;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Ürünün fiyatını giriniz : ");
+        fiyat = inp.nextDouble();
+
+        boolean kosul1 = fiyat<1000;
+        double sart = kosul1 ? 0.18 : 0.08;
+        double kdvfiyati = fiyat*sart;
+        System.out.println("Ürünün KDV Oranı : "+sart);
+        System.out.println("Ürünün KDV tutarı : "+kdvfiyati);
+        double hesap = fiyat+kdvfiyati;
+        System.out.println("Ürünün KDV dahil fiyatı : "+hesap);
+
+
+
+
+
+
+
+    }
+}
