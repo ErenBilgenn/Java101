ca5fdc41264f392311262ae1839d7b6c10532de6
Eren Bilgen <erenbilgenn@gmail.com>
2023-01-07 20:00:51 +0300

    Odev4  tamamlandi.

---
+++ src/Odev4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Odev4 {
+    public static void main(String[] args) {
+        int km;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Mesafeyi KM cinsinden giriniz : ");
+        km = inp.nextInt();
+        double para = (2.20*km)+10;
+        boolean kosul1 = para<20;
+        double sart = kosul1 ? 20 : (2.20*km)+10;
+
+        System.out.println("Taksimetre ücreti : "+sart);
+
+
+    }
+
+}
