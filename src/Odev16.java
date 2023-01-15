baab0c751889899a5b8643879d765c05aa7c3595
Eren Bilgen <erenbilgenn@gmail.com>
2023-01-15 17:37:57 +0300

    Odev16 tamamlandi

---
+++ src/Odev16.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Odev16 {
+    public static void main(String[] args) {
+        int yil;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Yıl Giriniz : ");
+        yil =inp.nextInt();
+
+        String artik = "";
+
+        if ((yil%100)==0){
+            if ((yil%400)==0){
+                artik = " bir artık yıldır !";
+            }else {
+                artik = " bir artık yıl değildir !";
+            }
+        } else if ((yil%4)==0) {
+            artik = " bir artık yıldır !";
+        }else {
+            artik = " bir artık yıl değildir !";
+        }
+        System.out.print(yil+artik);
+    }
+}
