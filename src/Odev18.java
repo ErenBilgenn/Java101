574b223a2492ff1e0fa5914bb6cbf342081e3047
Eren Bilgen <erenbilgenn@gmail.com>
2023-01-18 16:18:53 +0300

    Odev18 tamamlandi

---
+++ src/Odev18.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Odev18 {
+    public static void main(String[] args) {
+        int n, total = 0;
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Sayı Giriniz : ");
+            n = input.nextInt();
+            if ((n % 2 == 0) && (n % 4 == 0)) {
+                total += n;
+            }
+        } while (n % 2 == 0);
+        System.out.println("Çift ve 4'ün katları olan sayıların toplamı : " + total);
+    }
+}
