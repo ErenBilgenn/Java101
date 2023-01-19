f2c495e6f52bc0f6a5946d26a3501fa5e4b60ef2
Eren Bilgen <erenbilgenn@gmail.com>
2023-01-19 16:56:34 +0300

    Odev20 tamamlandi

---
+++ src/Odev20.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Odev20 {
+    public static void main(String[] args) {
+        int n, total1 = 1,total2 =1,total3 = 1,r;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("C(n,r) olmak üzere n sayısını giriniz : ");
+        n = scan.nextInt();
+
+        System.out.print("C(n,r) olmak üzere r sayısını giriniz : ");
+        r = scan.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            total1 *= i;
+        }
+        for (int i = 1; i <= r; i++) {
+            total2 *= i;
+        }
+        for (int i = 1; i <= n - r; i++) {
+            total3 *= i;
+        }
+        int result = total1 / (total2 * total3);
+
+        System.out.println("C(n,r) : " + result);
+    }
+}
