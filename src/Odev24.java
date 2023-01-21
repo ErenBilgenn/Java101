31fba91ad53c6ffcde7908b54aa0a17e6dfe50ce
Eren Bilgen <erenbilgenn@gmail.com>
2023-01-21 15:47:41 +0300

    Odev24 tamamlandi

---
+++ src/Odev24.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Odev24 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Basamak sayısını giriniz : ");
+        int n = inp.nextInt();
+
+        for (int i = 1; i <= (n/2)+1; i++) {
+
+            for (int k = 1; k <= ((n/2+1) - i); k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= n/2; i++){
+            for (int k = 1; k <= i;k++){
+                System.out.print(" ");
+            }
+            for (int j = 1;j <= (2*((n/2+1)-i)-1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
