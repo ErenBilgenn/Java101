5137a78f1c72bf736ab27391d1bf838c4bd8ccb2
Eren Bilgen <erenbilgenn@gmail.com>
2023-01-23 17:12:25 +0300

    Odev27 tamamlandi

---
+++ src/Odev27.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Odev27 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Kaç tane sayı gireceksiniz : ");
+        int n = inp.nextInt();
+        int n1, large = 0, small = 0;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + ". Sayıyı giriniz : ");
+            n1 = inp.nextInt();
+            if (n1 > large) {
+                large = n1;
+            }
+            if (i == 1 && n1 > 0) {
+                small = n1;
+            }
+            if (n1 < small) {
+                small = n1;
+            }
+        }
+        System.out.println("En büyük sayı : " + large);
+        System.out.println("En küçük sayı : " + small);
+    }
+}
+
