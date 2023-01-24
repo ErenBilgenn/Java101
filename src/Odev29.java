09224d9d256fd6530ec56835ec85f8e00bff7fdc
Eren Bilgen <erenbilgenn@gmail.com>
2023-01-24 18:50:43 +0300

    Odev29 tamamlandi.

--- src/Odev29.java
+++ src/Odev29.java
@@ -10,7 +10,7 @@ public class Odev29 {
             for (int k = 1; k <= i; k++) {
                 System.out.print(" ");
             }
-            for (int k = 1; k <= (n * 2 - (2 * i - 1)); k++) {
+            for (int j = 1; j <= (n * 2 - (2 * i - 1)); j++) {
                 System.out.print("*");
             }
             System.out.println();
