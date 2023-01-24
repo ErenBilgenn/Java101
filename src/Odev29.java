3fb472ae8c252239144559b1bb1afb7d98ccc326
Eren Bilgen <erenbilgenn@gmail.com>
2023-01-24 18:47:57 +0300

    Odev29 tamamlandi

---
+++ src/Odev29.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Odev29 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Basamak sayısı : ");
+        int n = inp.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (n * 2 - (2 * i - 1)); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
