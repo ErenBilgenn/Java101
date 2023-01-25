84155e036629dbbc544a4b109eec08ff122798fc
Eren Bilgen <erenbilgenn@gmail.com>
2023-01-25 16:06:39 +0300

    Odev31 tamamlandi

---
+++ src/Odev31.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Odev31 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("x elemanlı fibonacci serisi; x = ");
+        int x = inp.nextInt();
+        int next, step1 = 0, step2 = 1;
+
+        for (int i = 0; i < x; i++) {
+            if (i <= 1) {
+                next = i;
+            } else {
+                next = step1 + step2;
+                step1 = step2;
+                step2 = next;
+            }
+            System.out.print(next + " ");
+        }
+    }
+}
